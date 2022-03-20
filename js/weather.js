2de64cf267dae3dbc8673077394aaae4545e3f42
lokihyeon <hae8573@koreatech.ac.kr>
2022-03-20 17:16:57 +0900

    css style

--- js/weather.js
+++ js/weather.js
@@ -5,7 +5,6 @@ const API_KEY = "fcdd8643e9cc4edd0b15d836dc66c782";
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    console.log("you are here : ", lat, lon);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url)
         .then((response) => response.json())
